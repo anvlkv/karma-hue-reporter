6edd6439213dbef683bbed6fda7b2572f8dc62db
a.nvlkv <a.nvlkv@gmail.com>
2018-02-09 19:00:41 +0100

    updated tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,36 +1,6 @@
 /* global beforeEach, it, describe */
 
 'use strict';
-/*
- * Based upon "Function.prototype.bind polyfill for PhantomJS"
- *  author: Tom Watson <tom.james.watson@gmail.com>
- *  homepage: https://github.com/tom-james-watson/phantomjs-polyfill
- *
- *  This fixes a compatibility issue with running Phantom1 with Angular 1.5
- */
-
-/* jshint ignore:start */
-
-// if (typeof Function.prototype.bind != 'function') {
-//   Function.prototype.bind = function bind(obj) {
-//     var args = Array.prototype.slice.call(arguments, 1),
-//       self = this,
-//       nop = function() {
-//       },
-//       bound = function() {
-//         return self.apply(
-//           this instanceof nop ? this : (obj || {}), args.concat(
-//             Array.prototype.slice.call(arguments)
-//           )
-//         );
-//       };
-//     nop.prototype = this.prototype || {};
-//     bound.prototype = new nop();
-//     return bound;
-//   };
-// }
-
-/* jshint ignore:end */
 
 var rewire = require('rewire');
 var chai = require('chai');
@@ -42,20 +12,8 @@ var expect = chai.expect;
 var os = require('os');
 var reporterRewire = rewire('../index.js');
 var HueReporter = require('../index.js')['reporter:hue'];
+var HTTPRequest = require('HTTPRequest');
 
-// var ansiColors = {
-//   red: '\u001b[31m',
-//   yellow: '\u001b[33m',
-//   green: '\u001b[32m',
-//   reset: '\u001b[39m'
-// };
-// var windowsIcons = {
-//   success: '\u221A ',
-//   failure: '\u00D7 ',
-//   skipped: '- '
-// }
-
-//baseReporterDecorator functions
 var formatError = function (a, b) {
   return a + b;
 };
@@ -66,338 +24,38 @@ var baseReporterDecorator = function (context) {
   context.write = sinon.spy();
 };
 
+var config = {
+  hueReporter: {
+    ip: '123.234.234.2',
+    user:'user',
+    applyTo: 'lights',
+    applyToId: '1'
+  }
+}
+
 describe('HueReporter', function () {
-  // describe('when initializing', function () {
-  //   describe('and on a windows machine', function () {
-  //     function createSpecReporter(config) {
-  //       config = config || {};
-  //       var processMock = {
-  //         platform: function () {
-  //           return 'win32';
-  //         }
-  //       };
-  //       reporterRewire.__set__({
-  //         'reporter:spec': SpecReporter,
-  //         process: {
-  //           platform: 'win32'
-  //         }
-  //       });
-  //       return new reporterRewire['reporter:spec'][1](baseReporterDecorator, formatError, config);
-  //     };
-
-  //     it('SpecReporter should have icons defined appropriately', function () {
-  //       var newSpecReporter = createSpecReporter();
-  //       newSpecReporter.prefixes.success.should.equal(windowsIcons.success);
-  //       newSpecReporter.prefixes.failure.should.equal(windowsIcons.failure);
-  //       newSpecReporter.prefixes.skipped.should.equal(windowsIcons.skipped);
-  //     });
-
-  //     function createConfigWithPrefixes(prefixes) {
-  //       return {
-  //         specReporter: {
-  //           prefixes: prefixes
-  //         }
-  //       }
-  //     }
-  //     it('SpecReporter should allow overriding success icon only', function () {
-  //       var expected = 'PASS';
-  //       var config = createConfigWithPrefixes({ success: expected });
-  //       var newSpecReporter = createSpecReporter(config);
-  //       newSpecReporter.prefixes.success.should.equal(expected);
-  //       newSpecReporter.prefixes.failure.should.equal(windowsIcons.failure);
-  //       newSpecReporter.prefixes.skipped.should.equal(windowsIcons.skipped);
-  //     });
-
-  //     it('SpecReporter should allow overriding failure icon only', function () {
-  //       var expected = 'FAIL';
-  //       var config = createConfigWithPrefixes({ failure: expected });
-  //       var newSpecReporter = createSpecReporter(config);
-  //       newSpecReporter.prefixes.success.should.equal(windowsIcons.success);
-  //       newSpecReporter.prefixes.failure.should.equal(expected);
-  //       newSpecReporter.prefixes.skipped.should.equal(windowsIcons.skipped);
-  //     });
-
-  //     it('SpecReporter should allow overriding skipped icon only', function () {
-  //       var expected = 'SKIPPED';
-  //       var config = createConfigWithPrefixes({ skipped: expected });
-  //       var newSpecReporter = createSpecReporter(config);
-  //       newSpecReporter.prefixes.success.should.equal(windowsIcons.success);
-  //       newSpecReporter.prefixes.failure.should.equal(windowsIcons.failure);
-  //       newSpecReporter.prefixes.skipped.should.equal(expected);
-  //     });
-
-  //     it('SpecReporter should allow overriding all icons', function () {
-  //       var config = createConfigWithPrefixes({
-  //         skipped: 'Skipped',
-  //         failure: 'Failed',
-  //         success: 'Win!'
-  //       });
-  //       var expected = config.specReporter.prefixes;
-  //       var newSpecReporter = createSpecReporter(config);
-  //       newSpecReporter.prefixes.success.should.equal(expected.success);
-  //       newSpecReporter.prefixes.failure.should.equal(expected.failure);
-  //       newSpecReporter.prefixes.skipped.should.equal(expected.skipped);
-  //     });
-  //   });
-  //   describe('and colors are not defined', function () {
-  //     var newSpecReporter;
-  //     var config = {};
-
-  //     beforeEach(function () {
-  //       newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError, config);
-  //     });
-
-  //     it('SpecReporter should be defined appropriately', function () {
-  //       SpecReporter[0].should.equal('type');
-  //       newSpecReporter.should.be.a('object');
-  //     });
-
-  //     it('should set USE_COLORS to false by default', function () {
-  //       newSpecReporter.USE_COLORS.should.equal(false);
-  //     });
-  //   });
-  //   describe('and colors are defined', function () {
-  //     var newSpecReporter;
-  //     var config = {
-  //       colors: true
-  //     };
-
-  //     beforeEach(function () {
-  //       newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError, config);
-  //     });
-
-  //     it('SpecReporter should be defined appropriately', function () {
-  //       SpecReporter[0].should.equal('type');
-  //       newSpecReporter.should.be.a('object');
-  //     });
-
-  //     it('should set USE_COLORS to true', function () {
-  //       newSpecReporter.USE_COLORS.should.equal(true);
-  //     });
-
-  //     it('should set the BaseReporter function\'s colors', function () {
-  //       newSpecReporter.SPEC_FAILURE.should.equal(ansiColors.red + '%s %s FAILED' + ansiColors.reset + '\n');
-  //       newSpecReporter.SPEC_SLOW.should.equal(ansiColors.yellow + '%s SLOW %s: %s' + ansiColors.reset + '\n');
-  //       newSpecReporter.ERROR.should.equal(ansiColors.red + '%s ERROR' + ansiColors.reset + '\n');
-  //       newSpecReporter.FINISHED_ERROR.should.equal(ansiColors.red + ' ERROR' + ansiColors.reset);
-  //       newSpecReporter.FINISHED_SUCCESS.should.equal(ansiColors.green + ' SUCCESS' + ansiColors.reset);
-  //       newSpecReporter.FINISHED_DISCONNECTED.should.equal(ansiColors.red + ' DISCONNECTED' + ansiColors.reset);
-  //       newSpecReporter.X_FAILED.should.equal(ansiColors.red + ' (%d FAILED)' + ansiColors.reset);
-  //       newSpecReporter.TOTAL_SUCCESS.should.equal(ansiColors.green + 'TOTAL: %d SUCCESS' + ansiColors.reset + '\n');
-  //       newSpecReporter.TOTAL_FAILED.should.equal(ansiColors.red + 'TOTAL: %d FAILED, %d SUCCESS' + ansiColors.reset + '\n');
-  //     });
-  //   });
-
-  //   describe('and there are configurations set for the spec reporter', function () {
-  //     describe('and suppressFailed is truthy', function () {
-  //       var newSpecReporter;
-  //       var config = {};
-  //       beforeEach(function () {
-  //         config.specReporter = {
-  //           suppressFailed: true
-  //         };
-  //         newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError, config);
-  //       });
-
-  //       it('should return nothing for specSkipped', function () {
-  //         expect(newSpecReporter.specFailure()).to.equal();
-  //       });
-  //     });
-
-  //     describe('and suppressSkipped is truthy', function () {
-  //       var newSpecReporter;
-  //       var config = {};
-  //       beforeEach(function () {
-  //         config.specReporter = {
-  //           suppressSkipped: true
-  //         };
-  //         newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError, config);
-  //       });
-
-  //       it('should return nothing for specSkipped', function () {
-  //         expect(newSpecReporter.specSkipped()).to.equal();
-  //       });
-  //     });
-
-  //     describe('and suppressPassed is truthy', function () {
-  //       var newSpecReporter;
-  //       var config = {};
-  //       beforeEach(function () {
-  //         config.specReporter = {
-  //           suppressPassed: true
-  //         };
-  //         newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError, config);
-  //       });
-
-  //       it('should return nothing for specSuccess', function () {
-  //         expect(newSpecReporter.specSuccess()).to.equal();
-  //       });
-  //     });
-
-  //     describe('and suppressErrorSummary is truthy', function () {
-  //       var newSpecReporter;
-  //       var config = {};
-  //       beforeEach(function () {
-  //         config.specReporter = {
-  //           suppressErrorSummary: true
-  //         };
-  //         newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError, config);
-  //       });
-
-  //       it('should set the suppressErrorSummary flag to true', function () {
-  //         newSpecReporter.suppressErrorSummary.should.equal(true);
-  //       });
-  //     });
-
-  //     describe('and showSpecTiming is truthy', function () {
-  //       var newSpecReporter;
-  //       var config = {};
-  //       beforeEach(function () {
-  //         config.specReporter = {
-  //           showSpecTiming: true,
-  //         };
-  //         newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError, config);
-  //       });
-
-  //       it('should set the showSpecTiming flag to true', function () {
-  //         newSpecReporter.showSpecTiming.should.equal(true);
-  //       });
-  //     });
-  //   });
-  // });
-
   describe('functionality', function () {
     describe('onRunComplete', function () {
-      // describe('with no browsers', function () {
-      //   var newSpecReporter;
-      //   var config = {};
+      var newHueReporter;
+      beforeEach(function () {
+        newHueReporter = new HueReporter[1](baseReporterDecorator, formatError, config);
+        newHueReporter.HTTPRequest = {put:sinon.spy()};
+      });
 
-      //   beforeEach(function () {
-      //     newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError, config);
+      it('shoul call api', function () {
+        newHueReporter.onRunComplete([], []);
+        newHueReporter.HTTPRequest.put.should.have.been.called;
+      });
 
-      //     newSpecReporter.currentSuite.push('suite name');
-      //     newSpecReporter.onRunComplete([], []);
-      //   });
+      it('shoul set greeen when all passing', function () {
+        newHueReporter.onRunComplete([], {failed: 0, success: 1});
+        newHueReporter.HTTPRequest.put.should.have.been.calledWith(`http://${config.hueReporter.ip}/api/${config.hueReporter.user}/${config.hueReporter.applyTo}/${config.hueReporter.applyToId}/state`, '{"on":true, "hue":25500}');
+      });
 
-      //   it('should reset failures and currentSuite arrays', function () {
-      //     newSpecReporter.currentSuite.length.should.equal(0);
-      //     newSpecReporter.failures.length.should.equal(0);
-      //   });
-
-      //   it('should call writeCommonMsg', function () {
-      //     newSpecReporter.writeCommonMsg.should.have.been.called;
-      //   });
-
-      //   it('should call write', function () {
-      //     newSpecReporter.write.should.have.been.called;
-      //   });
-      // });
-
-      // describe('with browsers', function () {
-      //   describe('and there are no failures', function () {
-      //     var newSpecReporter;
-      //     var config = {};
-
-      //     beforeEach(function () {
-      //       newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError, config);
-      //       newSpecReporter.currentSuite.push('suite name');
-      //       newSpecReporter.onRunComplete(['testValue'], {
-      //         disconnected: false,
-      //         error: false,
-      //         failed: 0,
-      //         success: 10
-      //       });
-      //     });
-
-      //     it('should call to write all of the successful specs', function () {
-      //       newSpecReporter.write.should.have.been.calledWith(undefined, 10);
-      //     });
-
-      //     it('should reset failures and currentSuite arrays', function () {
-      //       newSpecReporter.currentSuite.length.should.equal(0);
-      //       newSpecReporter.failures.length.should.equal(0);
-      //     });
-
-      //     it('should call writeCommonMsg', function () {
-      //       newSpecReporter.writeCommonMsg.should.have.been.called;
-      //     });
-      //   });
-
-      //   describe('and there are failures', function () {
-      //     describe('and suppressErrorSummary is true', function () {
-      //       var newSpecReporter;
-      //       var config = {
-      //         specReporter: {
-      //           suppressErrorSummary: true
-      //         }
-      //       };
-      //       beforeEach(function () {
-      //         newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError, config);
-      //         newSpecReporter.logFinalErrors = sinon.spy();
-      //         newSpecReporter.currentSuite.push('suite name');
-      //         newSpecReporter.onRunComplete(['testValue'], {
-      //           disconnected: false,
-      //           error: false,
-      //           failed: 10,
-      //           success: 0
-      //         });
-      //       });
-
-      //       it('should call to write all of the failed and successful specs', function () {
-      //         newSpecReporter.write.should.have.been.calledWith(undefined, 10, 0);
-      //       });
-
-      //       it('should reset failures and currentSuite arrays', function () {
-      //         newSpecReporter.currentSuite.length.should.equal(0);
-      //         newSpecReporter.failures.length.should.equal(0);
-      //       });
-
-      //       it('should call writeCommonMsg', function () {
-      //         newSpecReporter.writeCommonMsg.should.have.been.called;
-      //       });
-
-      //       it('should not call to log the final errors', function () {
-      //         newSpecReporter.logFinalErrors.should.not.have.been.called;
-      //       });
-      //     });
-
-      //     describe('and suppressErrorSummary is false', function () {
-      //       var newSpecReporter;
-      //       var config = {};
-      //       beforeEach(function () {
-      //         newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError, config);
-      //         newSpecReporter.logFinalErrors = sinon.spy();
-      //         newSpecReporter.currentSuite.push('suite name');
-      //         newSpecReporter.onRunComplete(['testValue'], {
-      //           disconnected: false,
-      //           error: false,
-      //           failed: 10,
-      //           success: 0
-      //         });
-      //       });
-
-      //       it('should call to write all of the failed and successful specs', function () {
-      //         newSpecReporter.write.should.have.been.calledWith(undefined, 10, 0);
-      //       });
-
-      //       it('should reset failures and currentSuite arrays', function () {
-      //         newSpecReporter.currentSuite.length.should.equal(0);
-      //         newSpecReporter.failures.length.should.equal(0);
-      //       });
-
-      //       it('should call writeCommonMsg', function () {
-      //         newSpecReporter.writeCommonMsg.should.have.been.called;
-      //       });
-
-      //       it('should call to log the final errors', function () {
-      //         newSpecReporter.logFinalErrors.should.have.been.called;
-      //       });
-      //     });
-      //   });
-      // });
-      it('shoul set color to red when tests are failing', function () {
-        
-      })
+      it('shoul set red when all failing', function () {
+        newHueReporter.onRunComplete([], {failed: 1, success: 0});
+        newHueReporter.HTTPRequest.put.should.have.been.calledWith(`http://${config.hueReporter.ip}/api/${config.hueReporter.user}/${config.hueReporter.applyTo}/${config.hueReporter.applyToId}/state`, '{"on":true, "hue":0}');
+      });
     });
   });
 });
